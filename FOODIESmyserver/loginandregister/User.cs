eaa9d80091d2269fdc02aa1e65ec4e39bc4b71dd
Qura-tul-ain <quratulaink960@gmail.com>
2018-04-22 20:20:43 -0700

    server

---
+++ FOODIESmyserver/loginandregister/User.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace loginandregister
+{
+    public class User
+    {
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private string password;
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+        private string cpassword;
+
+        public string Cpassword
+        {
+            get { return cpassword; }
+            set { cpassword = value; }
+        }
+        private bool isadmin;
+
+        public bool Isadmin
+        {
+            get { return isadmin; }
+            set { isadmin = value; }
+        }
+        private bool iscashier;
+
+        public bool Iscashier
+        {
+            get { return iscashier; }
+            set { iscashier = value; }
+        }
+
+    }
+}
---
+++ FOODIESmyserver/loginandregister/Userdl.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Collections;
+
+
+
+namespace loginandregister
+{
+    public class Userdl
+    {
+        public static List<User> RegisterUser = new List<User>();
+
+        public static bool Registeration(string name,string password,string cpassword,bool isadmin,bool iscashier)
+        {
+            User obj = new User();
+
+            Boolean ac = false;
+            Boolean ab = false;
+            Boolean omg = false;
+            foreach (User b in Userdl.RegisterUser)
+            {
+                if (b.Name == name)
+                {
+                    ac = true;
+
+                }
+            }
+            if (ac)
+            {
+                return ac;
+            }
+
+
+            else
+            {
+
+                obj.Name = name;
+                obj.Password = password;
+                obj.Cpassword = cpassword;
+                obj.Isadmin = isadmin;
+                obj.Iscashier = iscashier;
+                if(obj.Password != obj.Cpassword)
+                {
+                    ab = true;
+                }
+                if (ab)
+                {
+                    return ab;
+
+                }
+                else
+                {
+                    Userdl.RegisterUser.Add(obj);
+                    return ac;
+                }
+               
+            }
+        }
+        public static bool login(string name,string password)
+        {
+            bool check = false;
+            foreach (User u in Userdl.RegisterUser)
+            {
+                if (u.Name==name && u.Password==password)
+                    check = true;
+            }
+            return check;
+        }
+
+        public static bool resetpassword(string name)
+        {
+            bool True = false;
+            foreach (User a in Userdl.RegisterUser)
+            {
+                if (a.Name ==name)
+                {
+                    // pass = true;
+                    a.Password = "123";
+                    True = true; 
+                }
+                else
+                {
+                    True = false;
+                }
+            }
+            return True;
+        }
+    }
+}
